faf5df6c2b3652946926a4eb730fe79630e2f0a0
Amrit-Raj12 <amrit.raj1224@gmail.com>
2024-12-10 13:06:54 +0530

    Dev- aded navigation on edit button

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,13 +3,15 @@ import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
 import useFetch from '../hooks/useFetch';
 import CoverOne from '../images/cover/cover-01.png';
 import userSix from '../images/user/user-06.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { UserProfile } from '../types/userTypes';
 import { GET_PROFILE_URL } from '../constants/apiUrl';
 
 const Profile = () => {
 
   const user = localStorage.getItem('user') || '{}';
+
+  const navigate = useNavigate();
 
   const {_id} = JSON.parse(user)
 
@@ -33,12 +35,12 @@ const Profile = () => {
             alt="profile cover"
             className="h-full w-full rounded-tl-sm rounded-tr-sm object-cover object-center"
           />
-          <div className="absolute bottom-1 right-1 z-10 xsm:bottom-4 xsm:right-4">
+          <div className="absolute bottom-1 right-1 z-10 xsm:bottom-4 xsm:right-4" onClick={() => navigate('/settings')}>
             <label
               htmlFor="cover"
               className="flex cursor-pointer items-center justify-center gap-2 rounded bg-primary py-1 px-2 text-sm font-medium text-white hover:bg-opacity-90 xsm:px-4"
             >
-              <input type="file" name="cover" id="cover" className="sr-only" />
+              {/* <input type="file" name="cover" id="cover" className="sr-only" /> */}
               <span>
               <svg width="18px" height="18px" className='stroke-current' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> <title /> <g id="Complete"> <g id="edit"> <g> <path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} /> <polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} /> </g> </g> </g> </g></svg>
 
